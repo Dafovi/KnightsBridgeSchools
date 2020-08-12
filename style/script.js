826cb6cd47ff16fbcadff99189b9ba7724cd27f7
Dafovi <swan364@hotmail.com>
2020-08-12 11:32:49 -0500

    Comentarios tour 360 KSI

--- style/script.js
+++ style/script.js
@@ -1,3 +1,11 @@
+
+function Mostrar(){ 
+    document.getElementById('instructivo').style.display="block";
+}
+function Ocultar(){
+    document.getElementById('instructivo').style.display="none";
+}
+var s = new Number(0);
 obtenerLocalStorage();
 function To(scene) {   
     var x = location.href = scene;
@@ -39,15 +47,27 @@ function salonesPre(n){
         document.getElementById('scnReception').setAttribute('visible', 'false');
     }
     if (n === 1){
+        document.getElementById('scnYear1').setAttribute('visible', 'false');
+        document.getElementById('scnYear2').setAttribute('visible', 'false');
+        document.getElementById('scnYear3').setAttribute('visible', 'false');
         document.getElementById('scnReception').setAttribute('visible', 'true');
     }
     if (n === 2){
+        document.getElementById('scnYear2').setAttribute('visible', 'false');
+        document.getElementById('scnYear3').setAttribute('visible', 'false');
+        document.getElementById('scnReception').setAttribute('visible', 'false');
         document.getElementById('scnYear1').setAttribute('visible', 'true');
     }
     if (n === 3){
+        document.getElementById('scnYear1').setAttribute('visible', 'false');
+        document.getElementById('scnYear3').setAttribute('visible', 'false');
+        document.getElementById('scnReception').setAttribute('visible', 'false');
         document.getElementById('scnYear2').setAttribute('visible', 'true');
     }
     if (n === 4){
+        document.getElementById('scnYear1').setAttribute('visible', 'false');
+        document.getElementById('scnYear2').setAttribute('visible', 'false');
+        document.getElementById('scnReception').setAttribute('visible', 'false');
         document.getElementById('scnYear3').setAttribute('visible', 'true');
     }
 }
@@ -78,6 +98,7 @@ function patioCaf(n){
     }
     if (n === 2){
         document.getElementById('scnCafeteria').setAttribute('visible', 'true');
+        document.getElementById('scnFutbol').setAttribute('visible', 'false');
     }
     if (n === 3){
         document.getElementById('scnVoleibol').setAttribute('visible', 'true');
@@ -85,6 +106,7 @@ function patioCaf(n){
         document.getElementById('scnBasquetbol').setAttribute('visible', 'false');
     }
     if (n === 4){
+        document.getElementById('scnCafeteria').setAttribute('visible', 'false');
         document.getElementById('scnFutbol').setAttribute('visible', 'true');
         document.getElementById('scnBasquetbol').setAttribute('visible', 'false');
         document.getElementById('scnVoleibol').setAttribute('visible', 'false');
@@ -107,13 +129,18 @@ function BachilleratoUno(n){
         document.getElementById('scnArtRoom').setAttribute('visible', 'false');
     }
     if (n === 1){
-        document.getElementById('scnMathematics').setAttribute('visible', 'true');
+        document.getElementById('scnMathematics').setAttribute('visible', 'true');        document.getElementById('scnMath&chemistry').setAttribute('visible', 'false');
+        document.getElementById('scnBiologia').setAttribute('visible', 'false');
     }
     if (n === 2){
         document.getElementById('scnMath&chemistry').setAttribute('visible', 'true');
+        document.getElementById('scnMathematics').setAttribute('visible', 'false');
+        document.getElementById('scnBiologia').setAttribute('visible', 'false');
     }
     if (n === 3){
         document.getElementById('scnBiologia').setAttribute('visible', 'true');
+        document.getElementById('scnMathematics').setAttribute('visible', 'false');
+        document.getElementById('scnMath&chemistry').setAttribute('visible', 'false');
     }
     if (n === 4){
         document.getElementById('scnScienceLab').setAttribute('visible', 'true');
@@ -133,15 +160,21 @@ function BachilleratoDos(n){
     }
     if (n === 1){
         document.getElementById('scnBusiness').setAttribute('visible', 'true');
+        document.getElementById('scnHistory').setAttribute('visible', 'false');
+        document.getElementById('scnTok').setAttribute('visible', 'false');
     }
     if (n === 2){
         document.getElementById('scnHistory').setAttribute('visible', 'true');
+        document.getElementById('scnBusiness').setAttribute('visible', 'false');
+        document.getElementById('scnTok').setAttribute('visible', 'false');
     }
     if (n === 3){
         document.getElementById('scnDanza').setAttribute('visible', 'true');
     }
     if (n === 4){
         document.getElementById('scnTok').setAttribute('visible', 'true');
+        document.getElementById('scnBusiness').setAttribute('visible', 'false');
+        document.getElementById('scnHistory').setAttribute('visible', 'false');
     }
 }
 
@@ -155,12 +188,21 @@ function PrimariaUno(n){
         document.getElementById('scnMac').setAttribute('visible', 'false');
     }
     if (n === 1){
+        
+        document.getElementById('scnYear5').setAttribute('visible', 'false');
+        document.getElementById('scnYear6').setAttribute('visible', 'false');
         document.getElementById('scnYear4').setAttribute('visible', 'true');
     }
     if (n === 2){
+        
+        document.getElementById('scnYear4').setAttribute('visible', 'false');
+        document.getElementById('scnYear6').setAttribute('visible', 'false');
         document.getElementById('scnYear5').setAttribute('visible', 'true');
     }
     if (n === 3){
+        
+        document.getElementById('scnYear4').setAttribute('visible', 'false');
+        document.getElementById('scnYear5').setAttribute('visible', 'false');
         document.getElementById('scnYear6').setAttribute('visible', 'true');
     }
     if (n === 4){
@@ -184,6 +226,7 @@ function PrimariaDos(n){
     }
     if (n === 1){
         document.getElementById('scnSpanish1').setAttribute('visible', 'true');
+         document.getElementById('scnEnglish').setAttribute('visible', 'false');
     }
     if (n === 2){
         document.getElementById('scnSpanish2').setAttribute('visible', 'true');
@@ -196,6 +239,7 @@ function PrimariaDos(n){
     }
     if (n === 5){
         document.getElementById('scnEnglish').setAttribute('visible', 'true');
+        document.getElementById('scnSpanish1').setAttribute('visible', 'false');
     }
     if (n === 6){
         document.getElementById('scnAuditorio').setAttribute('visible', 'true');
@@ -246,15 +290,167 @@ function obtenerLocalStorage(){
         let scena = localStorage.getItem("scn");
         console.log(scena);
         if(scena==="1"){
-            patioCaf(4);
+            patioCaf(4)
+            changeButton('caf',4)
             localStorage.setItem("scn",0)
         }
-        if(scena==="2"){
-            patioCaf(2);
+        else if(scena==="2"){
+            patioCaf(2)
             localStorage.setItem("scn",0)
         }
+        else if(scena==="3"){
+            PatioUno(1)
+            localStorage.setItem("scn",0)
+            changeButton('fuente',1)
+        }
+        else if(scena==="4"){
+            patioCaf(1)
+            localStorage.setItem("scn",0)
+        }
+        else if(scena==="5"){
+            BachilleratoUno(5)
+            localStorage.setItem("scn",0)
+        }else if(scena==="6"){
+            PrimariaDos(5);
+            localStorage.setItem("scn",0)
+        }else if(scena==="7"){
+            BachilleratoDos(4)
+            localStorage.setItem("scn",0)
+        }else if(scena==="8"){
+            BachilleratoUno(1)
+            changeButton('bachi1',4)
+            localStorage.setItem("scn",0)
+        }else if(scena==="9"){
+            PrimariaDos(8)
+            changeButton('prim2',4)
+            localStorage.setItem("scn",0)
+        }else if(scena==="10"){
+            patioCaf(2)
+            changeButton('caf',3)
+            localStorage.setItem("scn",0)
+        }else if(scena==="11"){
+            BachilleratoDos(3)
+            changeButton('bachi2',5)
+            localStorage.setItem("scn",0)
+        }
+        else if(scena==="12"){
+            BachilleratoUno(4)
+            changeButton('bachi1',9)
+            localStorage.setItem("scn",0)
+        }
+        
     }
     else{
         console.log("No hay entradas e el localstorage");
+    }
+}
+function changeButton(scene,n){
+    s+=n;
+    switch (scene){
+        case "fuente":
+            if(s<0)
+            To('administration.html');
+            else if(s>=0&&s<2)
+            PatioUno(s)
+            else
+            To('childPark.html')  
+        break;
+        case "pres":
+            if(s<0)
+            To('childPark.html') 
+            else if(s>=0&&s<5)
+            salonesPre(s)
+            else
+            To('primaryOne.html')
+        break;
+        case "prim1":
+            if(s<0)
+            To('preschool.html')
+            else if(s>=0&&s<4)
+            PrimariaUno(s)
+            else
+            GuardarLocalStorage(4,'coffeYard.html')
+        break;
+        case "bachi1":
+            //console.log("s=="+s);    
+            if(s<0)
+            GuardarLocalStorage(4,'coffeYard.html')
+            else if(s===0)
+            BachilleratoUno(5)
+            else if(s===1)
+            BachilleratoUno(0)
+            else if(s===2)
+            GuardarLocalStorage(6,'primaryTwo.html')
+            else if(s===3)
+            GuardarLocalStorage(7,'highSchoolTwo.html')
+            else if(s===4)
+            BachilleratoUno(1)
+            else if(s===5)
+            BachilleratoUno(2)
+            else if(s===6)
+            BachilleratoUno(3)
+            else if(s===7)
+            To('library.html')
+            else if(s===8)
+            GuardarLocalStorage(11,'highSchoolTwo.html')
+            else if(s===9)
+            BachilleratoUno(4)
+            else if(s===10)
+            To('parking.html')
+        break;
+        case "prim2":
+            if(s<0)
+            To('highSchoolOne.html')
+            else if(s===0)
+            PrimariaDos(5)
+            else if(s===1)
+            PrimariaDos(1)
+            else if(s===2)
+            GuardarLocalStorage(7,'highSchoolTwo.html')
+            else if(s===3)
+            To('library.html')
+            else if(s===4)
+            PrimariaDos(8)
+            else if(s===5)
+            GuardarLocalStorage(10,'coffeYard.html')
+        break;
+        case "bachi2":
+            if(s<0)
+            GuardarLocalStorage(6,'primaryTwo.html')
+            else if(s===0)
+            BachilleratoDos(4);
+            else if(s===1)
+            BachilleratoDos(2);
+            else if(s===2)
+            BachilleratoDos(1);
+            else if(s===3)
+            GuardarLocalStorage(8,'highSchoolOne.html')
+            else if(s===4)
+            GuardarLocalStorage(1,'coffeYard.html')   
+            else if(s===5)
+            BachilleratoDos(3)
+            else if(s===6)
+            GuardarLocalStorage(12,'highSchoolOne.html')            
+        break;
+        case "caf":
+            if(s<0)
+            To('primaryOne.html')
+            else if(s===0)
+            patioCaf(1);
+            else if(s===1)
+            GuardarLocalStorage(5,'highSchoolOne.html')
+            else if(s===2)
+            GuardarLocalStorage(9,'primaryTwo.html')
+            else if(s===3)
+            patioCaf(2)
+            else if(s>=4&&s<=5)
+            patioCaf(s)
+            else if(s===6)
+            patioCaf(3)
+            else if(s===7)
+            GuardarLocalStorage(11,'highSchoolTwo.html')
+        break;
+            
+        
     }
 }
